fb3d35a65a866293f1e94d80c006d94171e6c65d
CARLOS MALDONADO <karloz_0986@hotmail.com>
2025-05-13 16:13:16 -0700

    nada

--- controllers/empleados.controller.js
+++ controllers/empleados.controller.js
@@ -24,6 +24,30 @@ exports.getEmpleados = async (req, res) => {
     }
 }
 
+exports.getEmpleadosAsim = async (req, res) => {
+    try {
+        const db = getDb();
+
+        const empleados = await db.collection('mnom01h').find({ VALIDEZ: 'V'}, {
+            projection: {
+                _id: 0,
+                EMPLEADO: 1,
+                APPAT: 1,
+                APMAT: 1,
+                NOMBRE: 1,
+                RFC: 1,
+                CURP: 1,
+                EMAIL: 1
+            }
+        }).sort({ EMPLEADO: 1 }).toArray();
+
+        res.json(empleados);
+    } catch (error) {
+        console.error('Error al obtener empleados:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+}
+
 exports.getEmpleadoById = async (req, res) => {
     const { id } = req.params;
     try {
