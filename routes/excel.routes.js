99dbf209f419d2e51d860e42823aa7098a7fd635
CARLOS MALDONADO <karloz_0986@hotmail.com>
2025-05-16 13:34:25 -0700

    excel

--- routes/excel.routes.js
+++ routes/excel.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {generarExcel} = require('../controllers/excel.controller');
 
-router.get('/', generarExcel);  
+router.get('/:periodo/:tipo', generarExcel);  
 
 module.exports = router; 
