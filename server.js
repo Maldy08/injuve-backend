56f5be9f0f2a64acda70079ffbf916fed2396086
Carlos Maldonado <32904610+Maldy08@users.noreply.github.com>
2025-06-08 19:06:37 -0700

    nada

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const path = require('path');
 const jsreport = require('jsreport')({ extensions: { express: { enabled: false } } });
 const conectarMongo = require('./helpers/mongo.helper');
 
-// Rutas
-const authRoutes = require('./routes/auth.routes');
-const uploadRoutes = require('./routes/upload.routes');
-const nominaRoutes = require('./routes/nomina.routes');
-const pdfRoutes = require('./routes/pdf.routes');
-const sendEmailRoutes = require('./routes/send-email.routes');
-const empleadosRoutes = require('./routes/empleados.routes');
-const excelRoutes = require('./routes/excel.routes');
-const bssRoutes = require('./routes/bss.routes');
 const app = express();
 
 // Middlewares globales
@@ -21,29 +12,32 @@ app.use(cors());
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-const { swaggerUi, specs } = require('./swagger');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 const compression = require('compression');
 app.use(compression({
   filter: (req, res) => {
     if (req.headers.accept && req.headers.accept === 'text/event-stream') {
-      return false; // No comprimir SSE
+      return false;
     }
     return compression.filter(req, res);
   }
 }));
 
+// Documentación Swagger
+const { swaggerUi, specs } = require('./swagger');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-// Rutas
-app.use('/api/auth', authRoutes);
-app.use('/api/upload', uploadRoutes);
-app.use('/api/nomina', nominaRoutes);
-app.use('/api/pdf', pdfRoutes);
-app.use('/api/send-email', sendEmailRoutes);
-app.use('/api/empleados', empleadosRoutes);
-app.use('/api/timbrado', excelRoutes);
-app.use('/api/bss', bssRoutes);
+/* ✅ Agrupamos todas las rutas del backend en /api/backend para no interferir con Next.js */
+const baseApiPath = '/api/backend';
+
+// Rutas del backend
+app.use(`${baseApiPath}/auth`, require('./routes/auth.routes'));
+app.use(`${baseApiPath}/upload`, require('./routes/upload.routes'));
+app.use(`${baseApiPath}/nomina`, require('./routes/nomina.routes'));
+app.use(`${baseApiPath}/pdf`, require('./routes/pdf.routes'));
+app.use(`${baseApiPath}/send-email`, require('./routes/send-email.routes'));
+app.use(`${baseApiPath}/empleados`, require('./routes/empleados.routes'));
+app.use(`${baseApiPath}/timbrado`, require('./routes/excel.routes'));
+app.use(`${baseApiPath}/bss`, require('./routes/bss.routes'));
 
 // Inicialización del servidor con jsreport y MongoDB
 jsreport.init().then(() => {
@@ -52,5 +46,5 @@ jsreport.init().then(() => {
     console.log(`Servidor activo en http://localhost:${process.env.PORT || 3001}`);
   });
 }).catch(err => {
-  console.error(" Error al inicializar jsreport:", err);
+  console.error("Error al inicializar jsreport:", err);
 });
